a59bbd22907f4c3fc2286eadce59255ca44d5e56
Omar Alama <4943471+OasisArtisan@users.noreply.github.com>
2018-05-27 18:13:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import time
 try:
     import hardware_control
-    hardware_control.turn_on()
-    while True:
-        point = input("Enter the point to go to")
-        hardware_control.move_to_point(point)
+    hardware_control.turn_on(debugging=True, calibrate_first=True)
+    time.sleep(1000)
+    while False:
+        angle = input("Enter the angle\n")
+        if angle == 'i':
+            print ("\nCurrent_Angle: {}".format(hardware_control.current_angle))
+            print ("Wanted_Angle: {}".format(hardware_control.wanted_angle))
+            print ("Current_Point: {}".format(hardware_control.current_point))
+            print ("Wanted_Point: {}".format(hardware_control.wanted_point))
+        else:
+            hardware_control.rotate_to_angle(angle)
 except (KeyboardInterrupt, SystemExit):
     print("Shutting down!")
     hardware_control.turn_off()
